5a88270981dfa70f5e827d0e7347c74db5171471
Le_Van_Huy_Hoang <hoanglvhse170333@fpt.edu.vn>
2025-01-19 19:55:26 +0700

    edit profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,26 @@
-import Feed from './Feed'
-import { Outlet } from 'react-router-dom'
-import RightSidebar from './RightSidebar'
-import useGetAllPost from '@/hooks/useGetAllPost'
-import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers'
-import { useSelector } from 'react-redux'
+import Feed from "./Feed";
+import { Outlet } from "react-router-dom";
+import RightSidebar from "./RightSidebar";
+import useGetAllPost from "@/hooks/useGetAllPost";
+import useGetSuggestedUsers from "@/hooks/useGetSuggestedUsers";
+import { useSelector } from "react-redux";
 
 const Home = () => {
-    useGetAllPost();
-    useGetSuggestedUsers();
-    const { user } = useSelector(store => store.auth);
-    console.log('user', user);
-    return (
-        <div className='flex'>
-            <div className='flex-grow'>
-                <Feed />
-                <Outlet />
-            </div>
-            <div className='w-[350px]'>
-                <RightSidebar />
-            </div>
-        </div>
-    )
-}
+  useGetAllPost();
+  useGetSuggestedUsers();
+  const user = useSelector((store) => store.auth.user);
+  console.log("🚀 ~ Home ~ user:", user);
+  return (
+    <div className="flex">
+      <div className="flex-grow">
+        <Feed />
+        <Outlet />
+      </div>
+      <div className="w-[350px]">
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
