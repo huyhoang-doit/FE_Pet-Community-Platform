c7358236b6c26d5d0f90db8401ef7c192dd0d5cd
tai16092003 <ltt16092003gmail.com>
2025-01-21 14:51:24 +0700

    edit landing page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { setOnlineUsers } from "./redux/chatSlice";
 import { setLikeNotification } from "./redux/rtnSlice";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import "./App.css";
+import LandingPage from "./components/LandingPage";
 const browserRouter = createBrowserRouter([
   {
     path: "/",
@@ -65,6 +66,10 @@ const browserRouter = createBrowserRouter([
   {
     path: "/signup",
     element: <Signup />,
+  },
+  {
+    path: "/landing-page",
+    element: <LandingPage />,
   },
 ]);
 
